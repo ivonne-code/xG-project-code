120b9ad9834ec7ae811787f66d53ed5641886e1e
Yifeng Luo <yifengl4@illinois.edu>
2024-10-02 01:12:16 -0500

    4.03version

--- data_cleaning/csv_combing.py
+++ data_cleaning/csv_combing.py
@@ -1,31 +1,9 @@
 import pandas as pd
 
-def load_and_adjust_csv(file_path):
-    # Load the existing CSV file
-    df = pd.read_csv('argentina_player_stats_pt.csv')
-    
-    # Display the first few rows to understand what corrections are needed
-    print("Original Data:")
-    print(df.head())
-    
-    # Define the exact headers as they should appear based on your screenshot
-    correct_headers = [
-        'Player', '#', 'Pos', 'Age', 'Min', 'Att', 'Live', 'Dead', 'FK', 'TB', 'Sw', 'Crs', 'TI', 'CK', 'In', 'Out', 'Str', 'Cmp', 'Off', 'Blocks'
-    ]
-    
-    # Check if all correct headers are in the DataFrame, otherwise add them as empty columns
-    for header in correct_headers:
-        if header not in df.columns:
-            df[header] = ''
-
-    # Rearrange columns to match the exact order you need
-    df = df[correct_headers]
-    
-    # Save the adjusted DataFrame back to CSV
-    df.to_csv('adjusted_argentina_player_stats_pt.csv', index=False)
-    print("Adjusted Data:")
-    print(df.head())
-
-# Assuming the path to your CSV file
-csv_file_path = 'argentina_player_stats_pt.csv'
-load_and_adjust_csv(csv_file_path)
+file_path_pt = '/Users/yifengluo/Downloads/xG-project-code-main/xG-project-code/data_cleaning/web-scraping/Argentina_France_pt.csv'
+df_pt = pd.read_csv(file_path_pt)
+file_path_st = '/Users/yifengluo/Downloads/xG-project-code-main/xG-project-code/data_cleaning/web-scraping/shots_ap.csv'
+df_st = pd.read_csv(file_path_st)
+combined_df = pd.concat([df_pt, df_st], ignore_index=True)
+combined_df = combined_df.iloc[1:]
+combined_df = combined_df.dropna()
